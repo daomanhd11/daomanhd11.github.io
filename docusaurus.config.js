7c3a003c1b604ec545e52dcbdbcc6dc7b9a72215
AN_NGUYEN <121989734+daomanhd11@users.noreply.github.com>
2025-07-29 21:24:05 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,8 +27,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'daomanhd11', // Usually your GitHub org/user name.
+  projectName: 'daomanhd11.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
